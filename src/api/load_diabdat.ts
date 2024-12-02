4014b415b31ec84a6f4ad0614888da974036464c
Nikita Maximov <john.maks595@gmail.com>
2024-12-02 19:26:25 +0300

    Fixing cache issues

--- src/api/load_diabdat.ts
+++ src/api/load_diabdat.ts
@@ -6,15 +6,18 @@ const DiabdatSizes = [517501282, 263402726, 263383766];
 export { DiabdatSizes };
 
 export default async function load_diabdat(api: IApi, fs: IFileSystem) {
-	let file = fs.files.get("diabdat.mpq");
+	const fileName = "diabdat.mpq";
+	const lowerFileName = fileName.toLowerCase();
+
+	let file = fs.files.get(lowerFileName);
 	if (file && !DiabdatSizes.includes(file.byteLength)) {
-		fs.files.delete("diabdat.mpq");
-		await fs.delete("diabdat.mpq");
+		fs.files.delete(lowerFileName);
+		await fs.delete(fileName);
 		file = undefined;
 	}
 	if (!file) {
 		const diabdat = await axios.request({
-			url: import.meta.env.BASE_URL === "/" ? "diabdat.mpq" : import.meta.env.BASE_URL + "/diabdat.mpq",
+			url: import.meta.env.BASE_URL === "/" ? fileName : import.meta.env.BASE_URL + "/" + fileName,
 			responseType: "arraybuffer",
 			onDownloadProgress: (e) => {
 				if (api.onProgress) {
@@ -33,8 +36,8 @@ export default async function load_diabdat(api: IApi, fs: IFileSystem) {
 			throw Error("Invalid diabdat.mpq size. Try clearing cache and refreshing the page.");
 		}
 		const data = new Uint8Array(diabdat.data);
-		fs.files.set("diabdat.mpq", data);
-		fs.update("diabdat.mpq", data.slice());
+		fs.files.set(lowerFileName, data);
+		await fs.update(fileName, data.slice());
 	}
 	return fs;
 }
